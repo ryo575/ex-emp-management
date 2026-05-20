93d0aa7124ab1911a129c28e9cd0c02fbda86bb1
ryo575 <lingmuling31@gmail.com>
2026-05-20 12:01:38 +0900

    EmployeeRepositoryクラスの作成

---
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -0,0 +1,135 @@
+package com.example.repository;
+
+import com.example.domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+        Employee employee = new Employee();
+        employee.setId(rs.getInt("id"));
+        employee.setName(rs.getString("name"));
+        employee.setImage(rs.getString("image"));
+        employee.setGender(rs.getString("gender"));
+        employee.setHireDate(rs.getDate("hire_date"));
+        employee.setMailAddress(rs.getString("mail_address"));
+        employee.setZipCode(rs.getString("zip_code"));
+        employee.setAddress(rs.getString("address"));
+        employee.setTelephone(rs.getString("telephone"));
+        employee.setSalary(rs.getInt("salary"));
+        employee.setCharacteristics(rs.getString("characteristics"));
+        employee.setDependentsCount(rs.getInt("dependents_count"));
+        return employee;
+    };
+
+    /*
+        従業員一覧情報を入社日順（降順）で取得する
+        （従業員が存在しない場合はサイズ0件の従業員一覧を返す）
+
+        @return 選択された従業員一覧
+     */
+    public List<Employee> findAll() {
+        String sql = """
+                SELECT
+                    id,
+                    name,
+                    image,
+                    gender,
+                    hire_date,
+                    mail_address,
+                    zip_code,
+                    address,
+                    telephone,
+                    salary,
+                    characteristics,
+                    dependents_count
+                FROM
+                    employees
+                ORDER BY
+                    hire_date DESC
+                """;
+
+        List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+
+        return employeeList;
+    }
+
+    /*
+        主キーから従業員情報を取得する
+        従業員が存在しない場合にはSpringが例外を発生します
+
+        @param Integer id
+        @return　選択された従業員情報
+     */
+    public Employee findById(Integer id) {
+        String sql = """
+                SELECT
+                    id,
+                    name,
+                    image,
+                    gender,
+                    hire_date,
+                    mail_address,
+                    zip_code,
+                    address,
+                    telephone,
+                    salary,
+                    characteristics,
+                    dependents_count
+                FROM
+                    employees
+                WHERE
+                    id = :id
+                """;
+
+        SqlParameterSource param = new MapSqlParameterSource("id",id);
+        Employee employee = new Employee();
+
+        employee = template.queryForObject(sql,param,EMPLOYEE_ROW_MAPPER);
+
+        return employee;
+    }
+
+    /*
+        従業員情報を変更する
+        (idカラムを除いた従業員情報のすべてのカラムを更新できるようなSQLを発行する)
+
+        @param Employee employee
+
+     */
+    public void update(Employee employee){
+        SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+
+        String sql = """
+                UPDATE employees
+                SET
+                    name = :name,
+                    image = :image,
+                    gender = :gender,
+                    hire_date = :hireDate,
+                    mail_address = :mailAddress,
+                    zip_code = :zipCode,
+                    address = :address,
+                    telephone - :telephone,
+                    salary = :salary,
+                    characteristics = :characteristics,
+                    dependents_count = :dependentsCount
+                WHERE
+                    id = :id
+                """;
+
+        template.update(sql,param);
+    }
+
+}
