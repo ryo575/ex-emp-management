0ca409513972a46256de057c71c7c15866ad96ff
ryo575 <lingmuling31@gmail.com>
2026-05-20 11:08:46 +0900

    Administratorクラスの作成

---
+++ src/main/java/com/example/domain/Administrator.java
@@ -0,0 +1,28 @@
+package com.example.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Administrator {
+    /*
+    従業員ID
+     */
+    private Integer id;
+    /*
+    名前
+     */
+    private String name;
+    /*
+    メールアドレス
+     */
+    private String mailAddress;
+    /*
+    パスワード
+     */
+    private String password;
+}
