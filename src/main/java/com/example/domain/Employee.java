90e2a5b21520f2c9e3115a469911b214cbac96ef
ryo575 <lingmuling31@gmail.com>
2026-05-20 11:14:23 +0900

    Employeeクラスの作榮

---
+++ src/main/java/com/example/domain/Employee.java
@@ -0,0 +1,62 @@
+package com.example.domain;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+    /*
+    ID
+     */
+    private Integer id;
+    /*
+    名前
+     */
+    private String name;
+    /*
+    画像
+     */
+    private String image;
+    /*
+    性別
+     */
+    private String gender;
+    /*
+    入社日
+     */
+    private Date hireDate;
+    /*
+    メールアドレス
+     */
+    private String mailAddress;
+    /*
+    郵便番号
+     */
+    private String zipCode;
+    /*
+    住所
+     */
+    private String address;
+    /*
+    電話番号
+     */
+    private String telephone;
+    /*
+    給料
+     */
+    private Integer salary;
+    /*
+    特性
+     */
+    private String characteristics;
+    /*
+    扶養人数
+     */
+    private Integer dependentsCount;
+}
