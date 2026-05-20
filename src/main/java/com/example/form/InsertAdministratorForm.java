cf5a42579a90a79eab48086fd2762f8b537b3d28
ryo575 <lingmuling31@gmail.com>
2026-05-20 12:08:33 +0900

    AdministratorサービスクラスとInsertAdministratoeFormクラスの作成

---
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -0,0 +1,16 @@
+package com.example.form;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class InsertAdministratorForm {
+
+    private String name;
+    private String mailAddress;
+    private String password;
+}
