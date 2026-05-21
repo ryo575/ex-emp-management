925d154fb8dd62166b4c09436b6fa9c1b00e6387
ryo575 <lingmuling31@gmail.com>
2026-05-21 16:29:03 +0900

    扶養人数を未入力時にエラーを吐くように調整

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -6,9 +6,12 @@ import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -59,7 +62,17 @@ public class EmployeeController {
      * @return 従業員一覧画面
      */
     @PostMapping("/update")
-    public String update(UpdateEmployeeForm form) {
+    public String update(@Validated UpdateEmployeeForm form,
+                         BindingResult result,
+                         RedirectAttributes redirectAttributes,
+                         Model model) {
+        if (result.hasErrors()) {
+            Employee employee = employeeService.showDetail(Integer.parseInt(form.getId()));
+
+            model.addAttribute("employee", employee);
+            return "employee/detail";
+        }
+
         employeeService.updateDependentsCount(Integer.parseInt(form.getId()), form.getIntDependentsCount());
 
         return "redirect:/employee/show-list";
